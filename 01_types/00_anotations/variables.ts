eda01bbb9b7c752cb54fff4a7ae74e6582d3d7de
LPastine <lucianopastine.tech@gmail.com>
2022-02-21 20:38:45 -0300

    Delayed Initialization

--- 01_types/00_anotations/variables.ts
+++ 01_types/00_anotations/variables.ts
@@ -28,3 +28,11 @@ let point: { x: number; y: number} = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i)
 }
+
+// When to use annotations
+// 1) Function that returns the any type
+const json = '{"x": 10, "y": 20}'
+// We define the types of the variables the function - JSON.parse() - is going to return, so that the variable knows 
+// which type it's going to receive
+const coordinates: { x: number, y: number} = JSON.parse(json)
+console.log(coordinates) // {"x": 10, "y": 20}
